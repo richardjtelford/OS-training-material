c73aa0043191a1e377936b89ba1f65eefcc10ace
Matthew Grainger <DrMattG@users.noreply.github.com>
2023-11-12 09:10:19 +0100

    Add update 2023 data handling

---
+++ OS_2023/dataHandling/R/dogData_sinew.R
@@ -0,0 +1,19 @@
+#' @title DATASET_TITLE
+#' @description DATASET_DESCRIPTION
+#' @format A data frame with 195 rows and 12 variables:
+#' \describe{
+#'   \item{\code{Rank}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Breed}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Size}}{character COLUMN_DESCRIPTION}
+#'   \item{\code{Energy.Level}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Trainability.Level}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Affectionate.With.Family}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Good.With.Young.Children}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Good.With.Other.Dogs}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Openness.To.Strangers}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Watchdog.Protective.Nature}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Barking.Level}}{double COLUMN_DESCRIPTION}
+#'   \item{\code{Shedding.Level}}{double COLUMN_DESCRIPTION}
+#'}
+#' @details DETAILS
+"dogData"
